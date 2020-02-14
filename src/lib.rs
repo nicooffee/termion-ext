ba356869dfe06c651519ce3165a6323e39af97a7
nicooffee <nicolas.honorato@usach.cl>
2020-02-14 16:52:11 -0300

    w_box fixed

--- src/lib.rs
+++ src/lib.rs
@@ -31,8 +31,8 @@ impl<W: Write> AdvWrite for AlternateScreen<W>{
     fn w_box(&mut self,x: u16,y: u16,len_x: u16, len_y: u16,c_h: Option<char>,c_v: Option<char>){
         let c_h = match c_h{Some(x) => x,None => '―'};
         let c_v = match c_v{Some(x) => x,None => '│'};
-        self.w_line_h(x+1,y,len_x-2,c_h);
-        self.w_line_v(x,y+1,len_y-2,c_v);
+        self.w_line_h(x,y,len_x,c_h);
+        self.w_line_v(x,y,len_y,c_v);
         self.w_line_h(x,y+len_y-1,len_x,c_h);
         self.w_line_v(x+len_x-1,y,len_y,c_v);
         write!(self,"{}{}",cursor::Goto(x,y),'╭').unwrap();
