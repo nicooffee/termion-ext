82c1d6b2a12e44096c05e4fac50c8c5ef68af8aa
= <=>
2020-02-16 10:44:48 -0300

    added color

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,21 @@
 use std::io::Stdout;
 use termion::screen::AlternateScreen;
 use std::io::{ Write};
-use termion::{cursor};
+use termion::{cursor,color};
+
 pub trait AdvWrite {
     fn w_go_str(&mut self,x:u16,y:u16,string: &str);
     fn w_str_go(&mut self,x:u16,y:u16,string: &str);
+    fn w_go_str_color<T: color::Color>(&mut self,x:u16,y:u16,string: &str,fg: Option<T>,bg: Option<T>);
+    fn w_str_go_color<T: color::Color>(&mut self,x:u16,y:u16,string: &str,fg: Option<T>,bg: Option<T>);
     fn w_line_h(&mut self,x: u16,y: u16,n: u16,c: char) ;
     fn w_line_v(&mut self,x: u16,y: u16,n: u16,c: char) ;
     fn w_box(&mut self,x: u16,y: u16,len_x: u16, len_y: u16,c_h: Option<char>,c_v: Option<char>);
+    fn w_set_fg<T: color::Color>(&mut self,c: T);
+    fn w_set_bg<T: color::Color>(&mut self,c: T);
+    fn w_reset_color(&mut self);
 }
+
 impl<W: Write> AdvWrite for AlternateScreen<W>{
     
     fn w_go_str(&mut self,x:u16,y:u16,string: &str){
@@ -19,7 +26,19 @@ impl<W: Write> AdvWrite for AlternateScreen<W>{
     fn w_str_go(&mut self,x:u16,y:u16,string: &str) {
         write!(self,"{}{}",string,cursor::Goto(x,y)).unwrap();
     }
-
+    
+    fn w_go_str_color<T: color::Color>(&mut self,x:u16,y:u16,string: &str,fg: Option<T>,bg: Option<T>){
+        if let Some(color) = fg {self.w_set_fg(color)};
+        if let Some(color) = bg {self.w_set_bg(color)};
+        self.w_go_str(x,y,string);
+        self.w_reset_color();
+    }
+    fn w_str_go_color<T: color::Color>(&mut self,x:u16,y:u16,string: &str,fg: Option<T>,bg: Option<T>){
+        if let Some(color) = fg {self.w_set_fg(color)};
+        if let Some(color) = bg {self.w_set_bg(color)};
+        self.w_str_go(x,y,string);
+        self.w_reset_color();
+    }
 
     fn w_line_h(&mut self,x: u16,y: u16,n: u16,c: char){
         let (max_x,_):(u16,u16) = termion::terminal_size().unwrap();
@@ -49,6 +68,18 @@ impl<W: Write> AdvWrite for AlternateScreen<W>{
         write!(self,"{}{}",cursor::Goto(x+len_x-1,y),'╮').unwrap();
         write!(self,"{}{}",cursor::Goto(x,y+len_y-1),'╰').unwrap();
         write!(self,"{}{}",cursor::Goto(x+len_x-1,y+len_y-1),'╯').unwrap();
+    }
+
+    
+    fn w_set_fg<T: color::Color>(&mut self,c: T){
+        write!(self,"{}",color::Fg(c)).unwrap();
+    }
+    fn w_set_bg<T: color::Color>(&mut self,c: T){
+        write!(self,"{}",color::Bg(c)).unwrap();
+    }
+    fn w_reset_color(&mut self){
+        write!(self,"{}",color::Fg(color::Reset)).unwrap();
+        write!(self,"{}",color::Bg(color::Reset)).unwrap();
     }
 }       
 /*
