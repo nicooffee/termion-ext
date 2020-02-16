770f825dc7ce3efc79e8798954bba10ecd874c08
nicooffee <nicolas.honorato@usach.cl>
2020-02-16 10:56:47 -0300

    added color

--- src/lib.rs
+++ src/lib.rs
@@ -23,22 +23,28 @@ impl<W: Write> AdvWrite for AlternateScreen<W>{
     fn w_go_str(&mut self,x:u16,y:u16,string: &str){
         write!(self,"{}{}",cursor::Goto(x,y),string).unwrap();
     }
+
+
     fn w_str_go(&mut self,x:u16,y:u16,string: &str) {
         write!(self,"{}{}",string,cursor::Goto(x,y)).unwrap();
     }
     
+
     fn w_go_str_color<T: color::Color>(&mut self,x:u16,y:u16,string: &str,fg: Option<T>,bg: Option<T>){
         if let Some(color) = fg {self.w_set_fg(color)};
         if let Some(color) = bg {self.w_set_bg(color)};
         self.w_go_str(x,y,string);
         self.w_reset_color();
     }
+
+
     fn w_str_go_color<T: color::Color>(&mut self,x:u16,y:u16,string: &str,fg: Option<T>,bg: Option<T>){
         if let Some(color) = fg {self.w_set_fg(color)};
         if let Some(color) = bg {self.w_set_bg(color)};
         self.w_str_go(x,y,string);
         self.w_reset_color();
     }
+
 
     fn w_line_h(&mut self,x: u16,y: u16,n: u16,c: char){
         let (max_x,_):(u16,u16) = termion::terminal_size().unwrap();
@@ -48,6 +54,8 @@ impl<W: Write> AdvWrite for AlternateScreen<W>{
             write!(self,"{}",c).unwrap();
         }
     }
+
+
     fn w_line_v(&mut self,x: u16,y: u16,n: u16,c: char) {
         let (_,max_y):(u16,u16) = termion::terminal_size().unwrap();
         if y+n>max_y {()}
@@ -57,6 +65,7 @@ impl<W: Write> AdvWrite for AlternateScreen<W>{
         }
     }
     
+
     fn w_box(&mut self,x: u16,y: u16,len_x: u16, len_y: u16,c_h: Option<char>,c_v: Option<char>){
         let c_h = match c_h{Some(x) => x,None => '―'};
         let c_v = match c_v{Some(x) => x,None => '│'};
@@ -74,13 +83,19 @@ impl<W: Write> AdvWrite for AlternateScreen<W>{
     fn w_set_fg<T: color::Color>(&mut self,c: T){
         write!(self,"{}",color::Fg(c)).unwrap();
     }
+
+
     fn w_set_bg<T: color::Color>(&mut self,c: T){
         write!(self,"{}",color::Bg(c)).unwrap();
     }
+
+
     fn w_reset_color(&mut self){
         write!(self,"{}",color::Fg(color::Reset)).unwrap();
         write!(self,"{}",color::Bg(color::Reset)).unwrap();
     }
+
+    
 }       
 /*
      ╭――╮  
